5c934d7c937f657af10ebbef76a3456e492a0199
afeiship <1290657123@qq.com>
2017-08-30 13:35:43 +0800

    issue: optimze script

---
+++ build/webpack.dll-dev.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack';
+import AssetsWebpackPlugin from 'assets-webpack-plugin';
+import pkg from '../package.json';
+
+
+export default {
+  output: {
+    path: './dist/vendors',
+    filename: '[name].[hash].js',
+    library: '[name]_library'
+  },
+  entry: {
+    vendors: pkg.config.dllVendors
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      path: path.resolve(__dirname, pkg.config.dllManifest),
+      name: '[name]_library'
+    }),
+    new AssetsWebpackPlugin({
+      filename: 'bundle-config.json',
+      path: './dist/vendors'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // new webpack.IgnorePlugin(/React/),
+    // new webpack.IgnorePlugin(/ReactDOM/),
+  ]
+};
+
+
